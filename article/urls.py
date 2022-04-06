8fdc7b6f6b43e6d1aba03dbdac64eace6c74f1a0
huzhengyong <415857148@qq.com>
2022-04-06 21:10:26 +0800

    first commit

---
+++ article/urls.py
@@ -0,0 +1,29 @@
+from django.urls import path
+from article import views
+
+
+# 正在部署的应用的名称
+app_name = 'article'
+
+urlpatterns = [
+    path('article-list/', views.article_list, name='article_list'),
+    path('article-detail/<int:id>/', views.article_detail, name='article_detail'),
+    # 写文章
+    path('article-create/', views.article_create, name='article_create'),
+    # 删除文章
+    path('article-delete/<int:id>/', views.article_delete, name='article_delete'),
+    # 安全删除文章
+    path(
+        'article-safe-delete/<int:id>/',
+        views.article_safe_delete,
+        name='article_safe_delete'
+    ),
+    # 更新文章
+    path('article-update/<int:id>/', views.article_update, name='article_update'),
+    # 点赞 +1
+    path(
+        'increase-likes/<int:id>/',
+        views.IncreaseLikesView.as_view(),
+        name='increase_likes'
+    ),
+]
